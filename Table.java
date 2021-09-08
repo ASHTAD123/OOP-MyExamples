d1657d7ae9ce943e5f1354e1049383c279cceb9f
ASHTAD IRANI <38254097+ASHTAD123@users.noreply.github.com>
2021-09-08 10:54:08 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,25 @@
+class Table {
+		
+		String brandName;
+		String tableType;
+		String color;
+		String material;
+
+		boolean isFoldable;
+		boolean hasWeels;
+		boolean isUnderWarranrty;
+		boolean isHandmade;
+		boolean isDesignerTable;
+		boolean isMultipurpose;
+		boolean hasColorOptions;
+
+		int numOfLegs;
+		int sizeOfTable;
+		int length;
+		int breadth;
+		int width;
+		int height;
+		int shelves;
+		int cost;
+		int warrantyPeriod;
+}
