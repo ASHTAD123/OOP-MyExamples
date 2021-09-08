9443e4b0cb29a474aca7f6f549598788690bacca
ASHTAD IRANI <38254097+ASHTAD123@users.noreply.github.com>
2021-09-08 12:57:42 +0530

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,20 @@
+class Building {
+
+		String buildingName;
+		String buildingLocation;
+		String buildingColor;
+		String nameOfBuilder;
+		String typeOfBuilding;
+		
+		boolean isNocApproved;
+		boolean hasAmenities;
+		boolean isEarthquakeProof;
+		
+
+		int floors;
+		int flats;
+		int members;
+		double flatCostPerSq;
+		int yearOfFormation;
+
+}
