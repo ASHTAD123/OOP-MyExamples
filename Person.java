a06528bc01d6d865c7001a106b53b384775af22d
ASHTAD IRANI <38254097+ASHTAD123@users.noreply.github.com>
2021-09-09 10:18:10 +0530

    Add files via upload
    IS-A Relation

---
+++ Person.java
@@ -0,0 +1,30 @@
+class Person {
+	
+	//remains same for all persons
+	static int eyes  = 2 ;
+	static int ears  = 2 ;
+	static int nose  = 1 ;
+	static int hands = 2 ;
+	static int legs  = 2 ;
+	
+	//differs for all persons
+	String name;
+	String middleName;
+	String surname;
+	String behaviour;
+	String voice;
+	String hairColor;
+	String personality;
+	String likes;
+	String dislikes;
+	String skinColor;
+	String bodyType;
+	String economicStatus;
+	String educationStatus;
+	String martialStatus;
+	Date dob;
+
+	int height;
+	int weight;
+
+}
