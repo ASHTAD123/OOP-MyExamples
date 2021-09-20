e868b720b08e4ee3dd7e2c25fffb71c80a52e392
ASHTAD IRANI <38254097+ASHTAD123@users.noreply.github.com>
2021-09-20 20:18:10 +0530

    Add files via upload

---
+++ OperatingSystem.java
@@ -0,0 +1,49 @@
+interface OperatingSystem {
+
+	 void powerOnMechanism();
+	 void powerOffMechanism();
+	 void bootingMechanism();
+	 void loginMechanism();
+	 void startingMechanism();
+}
+class windows implements OperatingSystem {
+
+		void powerOnMechanism(){
+			System.out.println(" Power on windows ");
+		}
+		void powerOffMechanism(){
+			System.out.println(" Power off windows ");
+		}
+		void bootingMechanism(){
+			System.out.println(" Booting windows ");
+		}
+		
+		void loginMechanism(){
+			System.out.println(" Login mechanism windows ");
+		}
+		
+		void startingMechanism(){
+			System.out.println(" starting mechanism windows ");
+		}
+}
+
+class apple implements OperatingSystem {
+
+		void powerOnMechanism(){
+			System.out.println(" Power on apple ");
+		}
+		void powerOffMechanism(){
+			System.out.println(" Power off apple ");
+		}
+		void bootingMechanism(){
+			System.out.println(" Booting apple ");
+		}
+		
+		void loginMechanism(){
+			System.out.println(" Login mechanism apple ");
+		}
+		
+		void startingMechanism(){
+			System.out.println(" starting mechanism apple ");
+		}
+}
