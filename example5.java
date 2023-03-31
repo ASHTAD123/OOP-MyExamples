84dea916ae7f846eece3a0ee48c881b7b299c22e
ASHTAD IRANI <38254097+ASHTAD123@users.noreply.github.com>
2023-03-31 14:34:56 +0530

    Add files via upload

---
+++ example5.java
@@ -0,0 +1,22 @@
+/*
+ *  Abstract class which doesn't have any abstract method , can only be inherited but
+ *  not instantiated
+ */
+abstract class example5{
+
+}
+
+class ab extends example5{
+
+   
+}
+
+class c {
+    
+    public static void main(String[] args) {
+
+        example5 ob = new example5();
+        
+    }
+
+}
